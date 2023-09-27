25c4c9e937bbe04f16542cc4577e50465f5607f6
Viktor <Burenin777@gmail.com>
2023-09-27 14:27:08 -0400

    Initial Commit

---
+++ OnboardingTest/OnboardingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingTestApp.swift
+//  OnboardingTest
+//
+//  Created by Viktor on 2023-09-27.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
