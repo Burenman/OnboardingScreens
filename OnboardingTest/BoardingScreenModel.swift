f63ccc66d779bcbc69f0c35bfa0588c90ba0cebd
Viktor <Burenin777@gmail.com>
2023-09-27 15:00:40 -0400

    Updated Onboarding files, so they run well

---
+++ OnboardingTest/BoardingScreenModel.swift
@@ -0,0 +1,31 @@
+//
+//  BoardingScreenModel.swift
+//  OnboardingTest
+//
+//  Created by Viktor on 2023-09-27.
+//
+
+import Foundation
+import SwiftUI
+
+struct BoardingScreen: Identifiable {
+    var id = UUID().uuidString
+    var image: String
+    var title: String
+    var description: String
+}
+
+// Same Title and desc...
+let title = "Easy Payments with \nWalletory"
+let description = "Small business can receive device \npayment super fast and super easy"
+
+// Since image name and BG color name are same....
+
+// Sample Model SCreens....
+var boardingScreens: [BoardingScreen] = [
+
+    BoardingScreen(image: "screen1", title: title, description: description),
+    BoardingScreen(image: "screen2", title: title, description: description),
+    BoardingScreen(image: "screen3", title: title, description: description),
+    BoardingScreen(image: "screen4", title: title, description: description),
+]
--- OnboardingTest/ContentView.swift
+++ OnboardingTest/ContentView.swift
@@ -8,17 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var offset: CGFloat = .zero
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        
+        Onboarding()
+        
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
+
+extension View {
+    
+    func getScreenBounds() -> CGRect {
+        return UIScreen.main.bounds
+    }
+    
+}
+
